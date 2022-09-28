ccec4bcec25f57848a709b0fc4c72d6767be6364
HP <HP@aayu>
2022-09-28 12:27:11 +0530

    First Commit

---
+++ Day-1_ConnectingEclipseWithGitHub/src/org/tnsindia/basic/TestingEclipseForGitHub.java
@@ -0,0 +1,11 @@
+package org.tnsindia.basic;
+//program on to print the statement ---single line comment
+public class TestingEclipseForGitHub {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to TNS India Foundation");
+
+	}
+
+}
