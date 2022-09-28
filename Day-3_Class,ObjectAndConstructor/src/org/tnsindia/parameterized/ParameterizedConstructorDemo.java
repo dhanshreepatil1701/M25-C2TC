900a210f15cee734207ab6370f717a413758d303
HP <HP@aayu>
2022-09-28 23:46:39 +0530

    Third Commit

---
+++ Day-3_Class,ObjectAndConstructor/src/org/tnsindia/parameterized/ParameterizedConstructorDemo.java
@@ -0,0 +1,25 @@
+package org.tnsindia.parameterized;
+
+import java.util.Scanner;
+
+//Demo on parameterizedConstructor
+class Bike
+{
+	//variables
+	public int speed;
+	//default constructor
+	Bike(int speed)
+	{
+		this.speed=speed;
+		System.out.println("Speed: "+speed+"km/hr");
+	}
+}
+public class ParameterizedConstructorDemo {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter the value of speed: ");
+		int speed=s.nextInt();
+		Bike b=new Bike(speed);
+		s.close();
+	}
+}
