dd793c65dc97ca0f164d647644c3851a27954286
HP <HP@aayu>
2022-10-31 13:32:01 +0530

    Fifteen Commit

--- Day-15_Generics/src/org/tnsindia/generics/GenericsAdvantageDemo.java
+++ Day-15_Generics/src/org/tnsindia/generics/GenericsAdvantageDemo.java
@@ -7,21 +7,25 @@ import java.util.List;
 public class GenericsAdvantageDemo {
 
 	public static void main(String[] args) {
-		List<Integer> obj=new LinkedList<>();
-		obj.add(12);
+		List<String>obj=new LinkedList<>();
+		obj.add("12");
+		
 		//1.Type safety
+		//obj.add("Java");
 		//obj.add("Dhanshree");
 		
 		System.out.println(obj);
+		
 		//2.Type-casting is not required
-		List <Integer>obj2=new ArrayList<>();
-		obj2.add(15);
+		//List <Integer>obj2=new ArrayList<>();
+		//obj2.add(15);
+		
 		//3. Compile-time checking
 		//obj2.add("15");
 		
-		@SuppressWarnings("unused")
-		Integer d=obj2.get(0);
-		System.out.println(obj2);
+		//@SuppressWarnings("unused")
+		//Integer d=obj2.get(0);
+		//System.out.println(obj2);
 
 	}
 
